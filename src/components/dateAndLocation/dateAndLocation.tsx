2ce99a2a7eb41d16d8061fd3c8c521a78ec50d36
ugur ozoz <ugurozoz@yahoo.com>
2021-03-20 18:16:37 +0300

    location icon added

---
+++ src/components/dateAndLocation/dateAndLocation.tsx
@@ -0,0 +1,29 @@
+import './dateAndLocation.css';
+
+interface dateAndLocation {
+  weatherDay: string;
+  weatherDate: string;
+  location: string;
+}
+
+const DateAndLocation: React.FC<dateAndLocation> = ({
+  weatherDay,
+  weatherDate,
+  location,
+}) => {
+  return (
+    <>
+      <div className='DateAndLocation'>
+        <div className='dateContainer'>
+          <span className='daySpan'>{weatherDay}</span>
+          <span className='dotSpan'>•</span>
+          <span className='dateSpan'>{weatherDate}</span>
+        </div>
+        <div className="locationContainer"><span>{location}</span></div>
+      </div>
+      
+    </>
+  );
+};
+
+export default DateAndLocation;
