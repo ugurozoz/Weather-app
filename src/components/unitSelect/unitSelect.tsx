11e705b74e1b7d015bfa77a2fddd412ecd7d459b
ugur ozoz <ugurozoz@yahoo.com>
2021-03-25 22:10:58 +0300

    temperature unit select buttons

--- src/components/unitSelect/unitSelect.tsx
+++ src/components/unitSelect/unitSelect.tsx
@@ -1,9 +1,22 @@
-const unitSelect: React.FC = () => { 
+import './unitSelect.css';
 
-  return <div>
-    <button>°C</button>
-    <button>°F</button>
-  </div>
+interface temperatureUnit {
+  unit: 'F' | 'C';
 }
+
+const unitSelect: React.FC<temperatureUnit> = ({ unit }) => {
+  const temperatureSelectButtons = ['F', 'C'].map(buttonText => {
+    const button = <button className={unit===buttonText ? 'active-unit':'' }>°{buttonText}</button>
+    return button
+
+  })
+
+
+  return (
+    <div className='unitSelect'>
+      {temperatureSelectButtons}
+    </div>
+  );
+};
 
 export default unitSelect;
