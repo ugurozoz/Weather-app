fdf4315ca80b14038804be9d2fbfe4b37c81aadb
ugur ozoz <ugurozoz@yahoo.com>
2021-03-29 01:23:40 +0300

    location api

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,13 @@
 import Current from './components/current/current';
 import ForecatsHighlights from './components/forecasts-highlights/forecasts-highlights';
+// import axios from 'axios';
 import './App.css';
 
 function App() {
+
+  // https://www.metaweather.com/api/location/search/?query=san
+
+
   return (
     <div className='App'>
       <main>
--- src/components/current/current.tsx
+++ src/components/current/current.tsx
@@ -1,13 +1,36 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import SearchPlacesButton from '../searchPlacesButton/searchPlacesButton';
 import CurrentLocationButton from '../currentLocationButton/currentLocationButton';
 import CurrentSituation from '../currentSituation/currentSituation';
 import DateAndLocation from '../dateAndLocation/dateAndLocation';
 import SearchPlacesForm from '../searchPlacesForm/searchPlacesForm';
+import axios from 'axios';
 import './current.css';
 
+
+
 const Current: React.FC = () => {
+  console.log("CURRENT COMPONENT")
+
   const [searchPlacevisible, setSearchPlacevisible] = useState(false);
+  useEffect(() => {
+    console.log("CURRENT RENDERED")
+  }, [searchPlacevisible])
+
+  console.log(searchPlacevisible)
+
+  const skipCorsURL = 'http://localhost:4152/' 
+  const apiBaseUrl = 'https://www.metaweather.com/api/location/search/?'
+  const apiQuery = 'query=SAN'
+  // console.log(`${skipCorsURL}${apiBaseUrl}${apiQuery}`)
+  axios
+      .get(`${skipCorsURL}${apiBaseUrl}${apiQuery}`)
+      .then((response) => {
+        console.log('SUCCES',response.data);
+      })
+      .catch((error) => {
+        console.log('AXIOS ERROR', error)
+      });
 
   const showSearch = () => {
     console.log('showSearch Called');
